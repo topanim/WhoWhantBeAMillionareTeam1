667c81202ebac8adec2d60ae06951b5cf9fb3295
topanim <gorogannisan641@gmail.com>
2024-03-03 20:55:53 +0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Who Whant Be A Millionare Team #1"
+include(":app")
+ 
