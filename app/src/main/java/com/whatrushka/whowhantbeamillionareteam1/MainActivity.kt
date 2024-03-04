2c1fd9ca7516d7dedaa20632d436e848a602e54e
karpo <Karpovmisa84@gmail.com>
2024-03-05 01:31:51 +0300

    изменение в гредал, добавление зависимости compose navigation, добавление NavHost в файле navigation, частичная логика навигации в приложении

--- app/src/main/java/com/whatrushka/whowhantbeamillionareteam1/MainActivity.kt
+++ app/src/main/java/com/whatrushka/whowhantbeamillionareteam1/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.whatrushka.whowhantbeamillionareteam1.ui.theme.WhoWhantBeAMillionareTeam1Theme
+import com.whatrushka.whowhantbeamillionareteam1.views.navigation.FirstStart
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    FirstStart()
                 }
             }
         }
--- app/src/main/java/com/whatrushka/whowhantbeamillionareteam1/views/navigation/Navigation.kt
+++ app/src/main/java/com/whatrushka/whowhantbeamillionareteam1/views/navigation/Navigation.kt
@@ -1,4 +1,33 @@
 package com.whatrushka.whowhantbeamillionareteam1.views.navigation
 
-class Navigation {
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "screen_1"  // экран по умолчанию (названия лучше сохранять в константах)
+    ) {
+        composable("screen_1"){
+            //просто функция компосабл в которой открывается разметка экрана
+        }
+        composable("screen_2"){
+
+        }
+        composable("screen_3"){
+
+        }
+    }
 }
+
+
+
+
+
+
