f5c5e6459ac036c87be9b5e51e19be70234f6325
topanim <gorogannisan641@gmail.com>
2024-03-07 20:22:47 +0300

    initial player added

--- app/src/main/java/com/whatrushka/whowhantbeamillionareteam1/MainActivity.kt
+++ app/src/main/java/com/whatrushka/whowhantbeamillionareteam1/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.whatrushka.whowhantbeamillionareteam1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.whatrushka.whowhantbeamillionareteam1.buisness.view_model.models.sound.impl.Player
 import com.whatrushka.whowhantbeamillionareteam1.ui.theme.WhoWhantBeAMillionareTeam1Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val mp = Player(applicationContext)
                     Greeting("Android")
+
+                    Log.d("m", "started")
+
+                    Column {
+                        Button(onClick = {
+                            mp.start()
+                        }) {
+                            Text(text = "onStart")
+                        }
+
+                        Button(onClick = {
+                            mp.correctAnswer()
+                        }) {
+                            Text(text = "onStart")
+                        }
+                    }
                 }
             }
         }
